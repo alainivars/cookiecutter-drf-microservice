cbf0f84d256536fa501dc5be105f5813fe11606f
Alain IVARS <alainivars@gmail.com>
2019-06-16 11:48:02 +0100

    remove unused import

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/rest/views.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/rest/views.py
@@ -7,7 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from {{cookiecutter.app_name}}.rest.serializers import FileToFilesystemSerializer, UserSerializer
+from {{cookiecutter.app_name}}.rest.serializers import FileToFilesystemSerializer
 
 
 """
